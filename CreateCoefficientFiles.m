c70ae07a8cca45a9c9a259abb88c71f5e4a4cfdb
StackMonkey <Malthehb@gmail.com>
2025-05-31 02:38:56 +0200

    New14MPS results

--- CreateCoefficientFiles.m
+++ CreateCoefficientFiles.m
@@ -13,6 +13,17 @@ for k = 1:9
     save(filename1);
 end
 %%
+for k = 1:10
+    load("Coefficients.mat");
+    foldername=sprintf('C:\\Users\\malth\\Desktop\\Vestas OpenFast\\Vestas OpenFAST\\openFAST\\5MW_Land_DLL_WTurb14MPSExtreme%1d\\5MW_Land_DLL_WTurb.fst',k);
+    FAST_InputFileName = foldername;
+    TMax = 3600;
+    fname1 = sprintf('init14MPSExtreme%1d.mat',k);
+    destination = "C:\Users\malth\Desktop\Vestas OpenFast\Vestas OpenFAST\simulinkModels";
+    filename1 = fullfile(destination,fname1);
+    save(filename1);
+end
+%%
 clc
 clear
     load("Coefficients.mat");
--- runSimulink.m
+++ runSimulink.m
@@ -14,6 +14,22 @@ destination = "C:\Users\malth\Desktop\Vestas OpenFast\RLWindTurbineControl\Simul
 filename1 = fullfile(destination,fname1);
 save(filename1,"CombinedPPOSimOUTLong");
 end
+%% PPO test: 14 MPS Extreme
+for i = 1:7
+filename=sprintf("init14MPSExtreme%1d.mat",i);
+load(filename);
+sim SystemSimulationPPONew;
+CombinedPPOSimOUT = cat(2,OutData,CostData,Observations,Actions);
+fname1 = sprintf('PPOagentResult14MPSExtreme%1d.mat',i);
+destination = "C:\Users\malth\Desktop\Vestas OpenFast\RLWindTurbineControl\SimulinkResults";
+filename1 = fullfile(destination,fname1);
+save(filename1,"CombinedPPOSimOUT");
+CombinedPPOSimOUTLong = DEL;
+fname1 = sprintf('PPOagentDELResults14MPSExtreme%1d.mat',i);
+destination = "C:\Users\malth\Desktop\Vestas OpenFast\RLWindTurbineControl\SimulinkResults";
+filename1 = fullfile(destination,fname1);
+save(filename1,"CombinedPPOSimOUTLong");
+end
 %%
 sim SystemSimulationNoRL;
 CombinedBaselineSimOut = cat(2,CostData,Observations);
